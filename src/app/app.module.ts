011b886ac5bb2459a86e69df6145f29374ddeeaf
Agustin Espana <agustinespania@gmail.com>
2022-10-15 08:40:39 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,36 @@ import { MatIconModule } from "@angular/material/icon";
 
 import { AppComponent } from "./app.component";
 import { ItemComponent } from "./item/item.component";
-import { TasksComponent } from './tasks/tasks.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { reducers, metaReducers } from './reducers';
+import { TasksComponent } from "./tasks/tasks.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
+import { EffectsModule } from "@ngrx/effects";
+import { reducers, metaReducers } from "./reducers";
 
 @NgModule({
-  declarations: [AppComponent, ItemComponent, TasksComponent, SidebarComponent],
-  imports: [BrowserModule, HttpClientModule, MatIconModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), EffectsModule.forRoot([]), StoreModule.forRoot(reducers, {
-      metaReducers
-    })],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        ItemComponent,
+        TasksComponent,
+        SidebarComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        MatIconModule,
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: environment.production,
+        }),
+        EffectsModule.forRoot([]),
+        StoreModule.forRoot(reducers, {
+            metaReducers,
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
