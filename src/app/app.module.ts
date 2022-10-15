1664700025ee069776411424d728ff49e795a57d
Agustin Espana <agustinespania@gmail.com>
2022-10-15 08:24:14 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,13 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent, ItemComponent, TasksComponent, SidebarComponent],
-  imports: [BrowserModule, HttpClientModule, MatIconModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), EffectsModule.forRoot([])],
+  imports: [BrowserModule, HttpClientModule, MatIconModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), EffectsModule.forRoot([]), StoreModule.forRoot(reducers, {
+      metaReducers
+    })],
   providers: [],
   bootstrap: [AppComponent],
 })
